aef5ecb9d02260c0287f28690b53d5b1b42ca398
Rgodha24 <rgodha24@brophybroncos.org>
2022-03-15 16:38:33 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="app">
+      <Navbar />
+
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
