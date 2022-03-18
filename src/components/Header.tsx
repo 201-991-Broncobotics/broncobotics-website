9970bfb0cf4848206170eebd330697a7ad75a2d8
Rgodha24 <rgodha24@brophybroncos.org>
2022-03-18 12:29:22 -0700

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
 import Navbar from './Navbar'
 
 interface HeaderProps {
     currentPage: string,
 }
 
+let a: React.ReactNode;
+
 function Header(props: HeaderProps) {
+
+
+
+  if (props.currentPage === 'home') {
+    a = <div className='bg-inherit'><div className='text-center text-white' style={{marginTop:'3rem', fontFamily: "Martel", fontSize: "7.5rem"}}>BroncoBotics</div><div className='text-center text-white' style={{marginTop:'-.75rem', fontFamily: "Martel", fontSize: "3rem"}}>Together to Create</div></div>
+  }
+
+  else if (props.currentPage === 'about') {
+    a = <div className='bg-inherit'><div className='text-center text-white' style={{marginTop:'3rem', fontFamily: "Martel", fontSize: "7.5rem"}}>BroncoBotics</div><div className='text-center text-white' style={{marginTop:'-.75rem', fontFamily: "Martel", fontSize: "3rem"}}>Together to Create</div></div>
+  }
+
+  else if (props.currentPage === 'photos') {
+    a = <div className='bg-inherit'><div className='text-center text-white' style={{marginTop:'3rem', fontFamily: "Martel", fontSize: "7.5rem"}}>BroncoBotics</div><div className='text-center text-white' style={{marginTop:'-.75rem', fontFamily: "Martel", fontSize: "3rem"}}>Together to Create</div></div>
+  }
+
+  else  (props.currentPage === 'join'); {
+    a = <div className='bg-inherit'><div className='text-center text-white' style={{marginTop:'3rem', fontFamily: "Martel", fontSize: "7.5rem"}}>BroncoBotics</div><div className='text-center text-white' style={{marginTop:'-.75rem', fontFamily: "Martel", fontSize: "3rem"}}>Together to Create</div></div>
+  }
+
+
   return (
     <>
-        <Navbar currentPage={props.currentPage}/>
-        <img src="/src/logo.svg" alt="broncobotics logo" className=' w-full h-10'/>
+      <Navbar currentPage={props.currentPage} aboveImageText= {a}/>
+      
+      
+        
 
     </>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,50 @@
 interface NavbarProps {
-    currentPage: string
+    currentPage: string, 
+    aboveImageText: React.ReactNode,
 }
 
 const Navbar = (props: NavbarProps) => {
     
-    let defaulClassName: string = "block py-2 pr-4 pl-3 hover:text-gray-400"
-    let choosePage: string = "text-red-600 "
-    console.log(choosePage + defaulClassName);
-    let aboutClass: string = (props.currentPage === "about" ? choosePage + defaulClassName : defaulClassName);
-    let competitionsClass: string = (props.currentPage === "competitions" ? choosePage + defaulClassName : defaulClassName);
-    let photosClass: string = (props.currentPage === "photos" ? choosePage + defaulClassName : defaulClassName);
-    let joinClass: string = (props.currentPage === "join" ? choosePage + defaulClassName : defaulClassName);
-    let homeClass: string = (props.currentPage === "home" ? "self-center text-xl font-semibold whitespace-nowrap ".concat(choosePage) : "self-center text-xl font-semibold whitespace-nowrap")
+    let defaulClassName: string = "block py-2 pr-4 pl-3 "
+    let otherPage: string = " hover:text-gray-300 text-white "
+    let choosePage: string = " text-red-500 hover:text-red-400 "
+    // console.log(choosePage + defaulClassName);
+    let aboutClass: string = (props.currentPage === "about" ? choosePage + defaulClassName : defaulClassName + otherPage);
+    let competitionsClass: string = (props.currentPage === "competitions" ? choosePage + defaulClassName : defaulClassName + otherPage);
+    let photosClass: string = (props.currentPage === "photos" ? choosePage + defaulClassName : defaulClassName + otherPage);
+    let joinClass: string = (props.currentPage === "join" ? choosePage + defaulClassName : defaulClassName + otherPage);
+    let homeClass: string = (props.currentPage === "home" ? "self-center text-xl font-semibold whitespace-nowrap ".concat(choosePage) : "self-center text-xl font-semibold whitespace-nowrap ".concat(otherPage))
     
     return (
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 ">
-            <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="/#" className="flex items-center">
-                    <img src="/src/logo.svg" className="mr-3 h-6 sm:h-10" alt="BroncoBotics Logo" />
-                    <span className={homeClass}>BroncoBotics</span>
-                </a>
-                <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-                <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium" >
-                    <li>
-                    <a href="/about" className={aboutClass}>About Broncobotics</a>
-                    </li>
-                    <li>
-                    <a href="/photos" className={photosClass}>Photos</a>
-                    </li>
-                    <li>
-                    <a href="/join" className={joinClass}>How to Join</a>
-                    </li>
-                </ul>
+        <div className='' style={{"backgroundImage":"url(\"/src/header-bg-opacity.png\")", height:"26rem", width: 'auto',"backgroundPosition":"center","backgroundRepeat":"no-repeat","backgroundSize":"cover","position":"relative"}}>
+            <nav className="px-2 sm:px-4 py-2.5 bg-inherit">
+                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                    <a href="/#" className="flex items-center">
+                        <img src="/src/logo.svg" className="mr-3 h-6 sm:h-10" alt="BroncoBotics Logo" />
+                        <span className={homeClass} style={{fontFamily: 'DM Serif Text',}}>BroncoBotics</span>
+                    </a>
+                    <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </button>
+                    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium" >
+                        <li>
+                        <a href="/about" className={aboutClass} style={{fontFamily: 'DM Serif Text',}}>About Broncobotics</a>
+                        </li>
+                        <li>
+                        <a href="/photos" className={photosClass} style={{fontFamily: 'DM Serif Text',}}>Photos</a>
+                        </li>
+                        <li>
+                        <a href="/join" className={joinClass} style={{fontFamily: 'DM Serif Text',}}>How to Join</a>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            {props.aboveImageText}
+        </div>
         )
 }
 
