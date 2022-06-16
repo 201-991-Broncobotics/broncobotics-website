cb287a1f88fc63ef6152be6fed095c50a0838b3e
rgodha24 <rgodha24@brophybroncos.org>
2022-06-16 11:23:11 -0700

    fixed type error

--- pages/api/members/memberPages.ts
+++ pages/api/members/memberPages.ts
@@ -1,17 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import type {MemberPageType} from "../../members/[member]";
 import memberPagesJson from "../../../public/memberpages.json";
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<MemberPageType | "DOES NOT EXIST">
- ) {
-    let title = req.query.title;
-    let memberPages: MemberPageType[] = memberPagesJson; 
-
-    let a = memberPages.find(memberPage => memberPage.title === title)
-
-    res.send(a == undefined ? "DOES NOT EXIST" : a);
-
+export interface MemberPageType {
+    title: string;
+    name: string;
+    graduatingYear: number;
+    currentTeams: string[];
+    social: {
+       instagram?: string;
+       email: string;
+       github?: string;
+       twitter?: string;
+       phoneNumber?: string;
+       website?: string;
+    };
  }
- 
+
+
+export default function handler(
+   req: NextApiRequest,
+   res: NextApiResponse<MemberPageType | "DOES NOT EXIST">
+) {
+   let title = req.query.title;
+   let memberPages: MemberPageType[] = memberPagesJson;
+
+   let a = memberPages.find((memberPage) => memberPage.title === title);
+
+   res.send(a == undefined ? "DOES NOT EXIST" : a);
+}
