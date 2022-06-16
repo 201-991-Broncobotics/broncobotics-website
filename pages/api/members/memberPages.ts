4531c4040ccfb8aa45e44b83d5ec81306b0f1867
rgodha24 <rgodha24@brophybroncos.org>
2022-06-16 16:33:40 -0700

    fixed api call

--- pages/api/members/memberPages.ts
+++ pages/api/members/memberPages.ts
@@ -2,29 +2,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 import memberPagesJson from "../../../public/memberpages.json";
 
 export interface MemberPageType {
-    title: string;
-    name: string;
-    graduatingYear: number;
-    currentTeams: string[];
-    social: {
-       instagram?: string;
-       email: string;
-       github?: string;
-       twitter?: string;
-       phoneNumber?: string;
-       website?: string;
-    };
- }
-
+   title: string;
+   name: string;
+   graduatingYear: number;
+   currentTeams: string[];
+   social: {
+      instagram?: string;
+      email: string;
+      github?: string;
+      twitter?: string;
+      phoneNumber?: string;
+      website?: string;
+   };
+}
 
 export default function handler(
    req: NextApiRequest,
-   res: NextApiResponse<MemberPageType | "DOES NOT EXIST">
+   res: NextApiResponse<MemberPageType | ["DOES NOT EXIST"]>
 ) {
    let title = req.query.title;
    let memberPages: MemberPageType[] = memberPagesJson;
 
    let a = memberPages.find((memberPage) => memberPage.title === title);
 
-   res.send(a == undefined ? "DOES NOT EXIST" : a);
+   res.send(a == undefined ? ["DOES NOT EXIST"] : a);
 }
