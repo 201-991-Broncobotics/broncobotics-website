02d6d99e6d4de139b5cf94d1267d184760bf2a9e
rgodha24 <rgodha24@brophybroncos.org>
2022-06-28 10:45:16 -0700

    fixed it

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
-   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-   authDomain: "broncobotics.vercel.app",
-   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+   apiKey: "AIzaSyBmvvJxjspVsKolGreRuqGBWXk36AhVCuQ",
+   authDomain: "broncobotics-website.firebaseapp.com",
+   projectId: "broncobotics-website",
+   storageBucket: "broncobotics-website.appspot.com",
+   messagingSenderId: "33579810293",
+   appId: "1:33579810293:web:2fe661f4116d8f4c71b4bc",
+   measurementId: "G-690LZ8842K"
 });
 
 export { app };
